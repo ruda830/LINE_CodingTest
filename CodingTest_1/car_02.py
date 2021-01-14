e8992d81b5563788c0938ec9ba102d2cc6008e85
ruda <sheimisky7150@yahoo.co.jp>
2021-01-15 02:46:05 +0900

    関数化した。

--- CodingTest_1/car_02.py
+++ CodingTest_1/car_02.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("taxi_record_2.csv")
+def load_calc(file):
+    df = pd.read_csv(file)
+    dfn = pd.DataFrame(df.values, columns=['時間', '走行距離'])
+    #print(df.columns) ->Index(['13:50:08.245', '0.0'], dtype='object')
+    loads = dfn['走行距離'].sum()
 
-dfn = pd.DataFrame(df.values, columns=['時間','走行距離'])
-print(df.columns)
-loads = dfn['走行距離'].sum()
-print(loads)
+    return loads
+
+
 """
 with open('taxi_record_2.csv') as f:
     lines = f.readlines()
