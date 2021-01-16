ecb0352b02eb6dc4f0d1f9f719215bef7f1cf081
ruda <sheimisky7150@yahoo.co.jp>
2021-01-17 01:09:33 +0900

    全てのテストデータが深夜かそれ以外か判定する機能を付けた。

--- CodingTest_3/taxi_datetime_test.py
+++ CodingTest_3/taxi_datetime_test.py
@@ -9,16 +9,29 @@ dfn = pd.DataFrame(df.values, columns=['a', 'b'])
 dfn['a'] = pd.to_datetime(dfn['a'], format='%H:%M:%S.%f')
 print(type(dfn['a']))
 print((dfn['a'][2]))
-from_midnight = '1900-01-01 22:00:00.000'
-
-to_midnight = '1900-01-02 05:00:00.000'
 
 from_midnight = datetime.datetime(1900, 0o1, 0o1, 22, 00, 00, 000)
-to_midnight_md = datetime.datetime(1900, 0o1, 0o2, 0o5, 00, 00, 000)
+to_midnight = datetime.datetime(1900, 0o1, 0o2, 0o5, 00, 00, 000)
+nums = len(dfn.index)
+dfn.loc[from_midnight <= dfn['a'], 'c'] = 'midnight'
+dfn.loc[from_midnight > dfn['a'], 'c'] = 'sun'
+print(dfn)
+"""
+dfn['c']
+for t in range(nums):
+    if from_midnight <= dfn['a'][t] <= to_midnight:
+        print('本データ夜')
+        dfn['c'][t] = dfn.extend([2])
+    else:
+        print('本データ昼')
+        dfn['c'][t] = dfn.extend([1])
+"""
+"""
 if from_midnight <= dfn['a'][2] <= to_midnight:
     print('本データ夜')
 else:
     print('本データ昼')
+"""
 #print(from_midnight)
 #nums = len(dfn.index) # print('このカラムの長さは：' + str(num))
 #for i in range(nums):
@@ -39,4 +52,3 @@ print(DFn['c'])
 date = '2017/10/19 10:54:29'
 print(dfn['a'][2] > datetime.date.today())
 """
-print(dfn)
