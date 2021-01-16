6a00b85778cf597d2ee2c6e1eb58b72ced4d33ce
ruda <sheimisky7150@yahoo.co.jp>
2021-01-16 18:22:26 +0900

    car_04.pyでindex毎の速度計算が出来た。

--- CodingTest_1/car_02.py
+++ CodingTest_1/car_02.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-
+import datetime
+"""
 def load_calc(file):
     df = pd.read_csv(file)
     dfn = pd.DataFrame(df.values, columns=['時間', '走行距離'])
@@ -8,14 +9,55 @@ def load_calc(file):
     loads = dfn['走行距離'].sum()
 
     return loads
-
+"""
 
 if __name__ == '__main__':
+    #本番時には消す↓
     df = pd.read_csv('taxi_record_2.csv')
     dfn = pd.DataFrame(df.values, columns=['時間', '走行距離'])
-    # print(df.columns) ->Index(['13:50:08.245', '0.0'], dtype='object')
-    loads = dfn['走行距離'].sum()
 
+
+
+    print(pd.to_datetime(dfn['時間']))
+    dfn['時間'] = pd.to_datetime(dfn['時間'])
+    df['b_diff'] = df['時間'].diff(-1)
+    print(dfn)
+
+
+    #dfn['jp'] = dfn['時間'][1].second
+    #print(dfn['時間'][1].second - dfn['時間'][0].second) #13:50:11.123
+
+"""
+    dfn['差分'] = 0
+    for m in dfn['走行距離']:
+        dfn['差分'].loc[m +1 ] = 10
+    print(dfn)
+    #dfn['差分'].append
+
+
+
+
+    #dfn['差分'] = []
+    #dfn['差分'].append = dfn['時間'][1] - dfn['時間'][0]
+    #for m in dfn:
+    #    dfn['差分'].append = dfn['時間'][m + 1] - dfn['時間'][m]
+
+    #print(dfn['時間'][i+1]-dfn['時間'][i] for i in range(len(dfn['時間'])-1))
+    #dfn['差分'] = (df.dtime[i+1] - df.dtime[i] for i in pd.to_datetime(dfn['時間']))
+    #dfn['差分'] = pd.to_datetime(dfn['時間'])/np.timedelta64(1, 's')
+    #for
+"""
+
+
+    #print(type(dfn['時間'][0]))
+    #str_time = str(dfn['時間'])
+
+    #文字列型から日付型に変える。
+    #before = '2017-05-23 12:47:23'　　　->'%Y-%m-%d %H:%M:%S'
+    #after = datetime.datetime.strptime(dfn['時間'], '%H:%M:%S:%f')
+
+    #print(type(before))
+    #print(type(after))
 
 
 
---
+++ CodingTest_1/car_04.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import datetime
+import numpy as np
+"""
+df = pd.DataFrame({'a': range(1, 6),
+                   'b': [x**2 for x in range(1, 6)],
+                   'c': [x**3 for x in range(1, 6)]})
+
+df['b_diff'] = df['b'].diff(-1)
+print(df)
+print(type(df))
+
+#    a   b    c  b_diff
+# 0  1   1    1    -3.0
+# 1  2   4    8    -5.0
+# 2  3   9   27    -7.0
+# 3  4  16   64    -9.0
+# 4  5  25  125     NaN
+"""
+#読み込み
+df = pd.read_csv('taxi_record_2.csv', header=None)
+dfn = pd.DataFrame(df.values, columns=['a', 'b'])
+#datetime型に変換
+dfn['a'] = pd.to_datetime(dfn['a'])
+#差分を取る
+dfn['a_diff'] = dfn['a'].diff(1) #-> 0 days 00:00:02.878000
+
+dfn['a_diff'] = dfn['a_diff'] / np.timedelta64(1, 's')
+
+dfn['speed'] = dfn['b']/dfn['a_diff']
+
+print(dfn)
+#DFN['a'] = pd.to_datetime['a']
+#df.time = pd.to_datetime(DFN['a'], format='%H%M%S%f')
+
+#DFN['a'] = datetime.datetime.strptime(DFN['a'])
+#DFN['a_diff'] = DFN['a'].diff(-1)
+
+
