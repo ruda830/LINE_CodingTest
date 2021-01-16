2cdb732184992d0ba019416cf702e8f4ee4d3f61
ruda <sheimisky7150@yahoo.co.jp>
2021-01-17 01:32:29 +0900

    全てのテストデータが深夜かそれ以外か判定し、深夜走行距離を求める機能を付けた。

--- CodingTest_3/taxi_datetime_test.py
+++ CodingTest_3/taxi_datetime_test.py
@@ -10,12 +10,31 @@ dfn['a'] = pd.to_datetime(dfn['a'], format='%H:%M:%S.%f')
 print(type(dfn['a']))
 print((dfn['a'][2]))
 
+#深夜時間帯を抽出
 from_midnight = datetime.datetime(1900, 0o1, 0o1, 22, 00, 00, 000)
 to_midnight = datetime.datetime(1900, 0o1, 0o2, 0o5, 00, 00, 000)
-nums = len(dfn.index)
+
 dfn.loc[from_midnight <= dfn['a'], 'c'] = 'midnight'
 dfn.loc[from_midnight > dfn['a'], 'c'] = 'sun'
+#dfn_midに記録
+dfn_mid = dfn.query("c=='midnight'")
+#深夜走行距離を算出
+midnight_loads = dfn_mid['b'].sum()
+print(dfn_mid)
+print(midnight_loads)
+
 print(dfn)
+"""
+midnight_Minutes = 0
+sun_time = 0
+if dfn['c'] == 'midnight':
+    midnight_Minutes += dfn['b']
+else:
+    sun_time += dfn['b']
+print(midnight_Minutes)
+print(sun_time)
+"""
+
 """
 dfn['c']
 for t in range(nums):
