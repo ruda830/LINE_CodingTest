db66699a8c4e393f240dde3d2e489a41ce60daa5
ruda <sheimisky7150@yahoo.co.jp>
2021-01-31 04:15:32 +0900

    コメント文字数を減らして修正。また、nums,teisoku_numsのところはfor文で回さずに掛け算に直した。

--- main/dfcalc.py
+++ main/dfcalc.py
@@ -6,47 +6,36 @@ import math
 
 class Record:
 
-    # データフレーム
+    # pandasでのDF加工に、時間と走行距離カラムを作製
     def make_df(self, file):
         df = pd.read_csv(file, header=None)
         dfn = pd.DataFrame(df.values, columns=['時間', '走行距離'])
         dfn['時間'] = pd.to_datetime(dfn['時間'], format='%H:%M:%S.%f')
         return dfn
 
-    # 総走行距離
     def load_calc(self, dfn):
         loads = dfn['走行距離'].sum()
         return loads
 
-    # 各スピード
+    # 低速運転チェックのため、speedカラムを設置
     def speed_calc(self, dfn):
-        # 日付カラムをdatetime型に変換
         dfn['時間'] = pd.to_datetime(dfn['時間'])
-        # 走行時間を逐一記録する
         dfn['時間_diff'] = dfn['時間'].diff(1)  # -> 0 days 00:00:02.878000
-        # 走行時間を秒数に変換
         dfn['時間_diff'] = dfn['時間_diff'] / np.timedelta64(1, 's')
-
-        # スピードを計算(m/s)
         dfn['speed'] = dfn['走行距離'] / dfn['時間_diff']
-
         return dfn['speed']
 
-    # 低速スピードの収集
+    # 低速運賃の計算用に、低速運行時の総時間を記録　(10km/h = 2.778m/s以下)
     def lowspeed_time(self, dfn):
-        # 低速運行の時に時間を記録する。10km/h = 2.778m/s以下。
         low_Minutes = 0
-
-        nums = len(dfn.index)  # print('このカラムの長さは：' + str(num))
+        nums = len(dfn.index)
         for i in range(nums):
             if dfn['speed'][i] <= 2.778:
                 low_Minutes += dfn['時間_diff'][i]
-        # 端数は切り捨て
         low_Minutes = math.ceil(low_Minutes)
-        # print('合計低速時間は：' + str(format(low_Minutes, '.3f') + '秒です。'))
         return low_Minutes
 
-    # 深夜時間帯を抽出
+    # 深夜帯運賃の計算用に、深夜時間帯の総距離を記録
     def midnight_load_bool(self, dfn):
         from_midnight = datetime.datetime(1900, 0o1, 0o1, 22, 00, 00, 000)
         #to_midnight = datetime.datetime(1900, 0o1, 0o2, 0o5, 00, 00, 000)
@@ -54,21 +43,6 @@ class Record:
         # 朝夜カラムにmidnjghtまたはsunの判定を記録
         dfn.loc[from_midnight <= dfn['時間'], '朝夜'] = 'midnight'
         dfn.loc[from_midnight > dfn['時間'], '朝夜'] = 'sun'
-        # midnightを抽出して、dfn_midに記録
         dfn_mid = dfn.query("朝夜=='midnight'")
-        # 深夜走行距離を算出
         midnight_loads = dfn_mid['走行距離'].sum()
-
         return midnight_loads
-
-"""
-if __name__ == '__main__':
-    record = Record()
-    record.make_df('record_1.csv')
-    print(record.make_df('record_1.csv'))
-    record.load_calc()
-    record.speed_calc()
-    record.lowspeed_time()
-    record.midnight_load_bool()
-
-"""
---
+++ main/main.py
@@ -0,0 +1,55 @@
+"""
+タクシー代を求めるには、
+下の../taxi_record/record_1.csvを適宜変更し、
+CUIでmain.pyを実行してください。
+$ python main.py
+
+レコードデータに対応した運賃が返されます。
+"""
+import dfcalc  # データフレーム生成及び、速度と距離算出
+import moneycalc  # 運賃計算
+
+if __name__ == '__main__':
+    try:
+        # ≺データ成形編:used class Record≻-------------------
+
+        # インスタンス生成
+        record = dfcalc.Record()
+        dfn_1 = record.make_df('../taxi_record/record_1.csv')
+        # 総走行距離を算出
+        all_load = record.load_calc(dfn_1)
+        print('総走行距離は：' + str(all_load) + 'mです。')
+        # 各スピードを計算、記録
+        every_speed = record.speed_calc(dfn_1)
+
+        # 低速スピードの時間を収集、記録
+        all_low_Minutes = record.lowspeed_time(dfn_1)
+        print('低速時間は：' + str(all_low_Minutes) + '秒です。')
+
+        # 深夜時間帯走行距離の収集、算出
+        all_midnight_loads = record.midnight_load_bool(dfn_1)
+        all_midnight_loads_calc = all_midnight_loads * 1.25
+        print('深夜時間帯走行距離は：' + str(all_midnight_loads) + 'mです。')
+        # print(dfn_1)
+
+        # ≺運賃計算編:used class Taxi≻-------------------
+
+        # インスタンス生成
+        taxi = moneycalc.Taxi()
+        # kasanの計算
+        taxi.kasan(all_load)
+        # teisokuの計算
+        taxi.teisoku(all_low_Minutes)
+        # sinyaの計算
+        taxi.kasan_sinya(all_midnight_loads, all_load)
+        # 合計
+        goukei = taxi.unchin()
+        print("総合計料金(初乗り＋加算＋低速＋深夜)は" + str(goukei) + "円です。")
+
+        # 終了の合図
+        taxi.taxi_stop()
+
+    except FileNotFoundError as e:
+        print("ファイルが見つかりません", e)
+    except Exception as e:
+        print("ファイルの読み込みエラーです", e)
